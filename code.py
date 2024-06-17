87a44c880516b75989d66b1586d368c43c7f12db
Ashu Desai <ashuddesai@gmail.com>
2024-06-16 17:29:28 -0700

    pre circuitpython update

--- code.py
+++ code.py
@@ -24,17 +24,17 @@ count = 0
 
 # Run loop
 while True:
-    audio.update()
-    midi.update()
+    # audio.update()
+    # midi.update()
     composer.update()
     if count % composer.speed*5 == 0:
         knobs.update()
     count+=1
 
-    # Update colors at specific intervals
-    if count % (composer.speed * 50) == 0:  # Every 50 iterations multiplied by the speed factor
-        new_colors = [(255, 255, 0), (0, 255, 255), (255, 0, 0)]  # Define new colors
-        composer.update_pattern_colors(new_colors)  # Update pattern colors
+    # # Update colors at specific intervals
+    # if count % (composer.speed * 50) == 0:  # Every 50 iterations multiplied by the speed factor
+    #     new_colors = [(255, 255, 0), (0, 255, 255), (255, 0, 0)]  # Define new colors
+    #     composer.update_pattern_colors(new_colors)  # Update pattern colors
 
     if count % (composer.speed * 5) == 0:  # Update knobs at specific intervals
         knobs.update()
--- phoenix/composition/composer.py
+++ phoenix/composition/composer.py
@@ -28,15 +28,16 @@ class Composer:
         #                  Pattern(TRIANGLE, 'inner_clockwise', 12, (70, 0, 200), (0, 0, 0)),
         #                  ]
         self.patterns = [
-                         Pattern(TRIANGLE, 'doc_james_1', 6, (35, 76, 130), (0, 0, 0)),
-                         Pattern(TRIANGLE, 'doc_james_2', 6, (35, 76, 130), (0, 0, 0)),
-                         Pattern(TRIANGLE, 'doc_james_3', 6, (35, 76, 130), (0, 0, 0)),
+                         Pattern(TRIANGLE, 'doc_james_1', 6, colors.amber, (0, 0, 0)),
+                         Pattern(TRIANGLE, 'doc_james_2', 6, colors.amber, (0, 0, 0)),
+                         Pattern(TRIANGLE, 'doc_james_3', 6, colors.amber, (0, 0, 0)),
         #                  Pattern(TRIANGLE, 'doc_james_4', 6, (35, 76, 130), (0, 0, 0)),
         #                  Pattern(TRIANGLE, 'doc_james_5', 3, (130, 30, 70), (0, 0, 0)),
         #                  Pattern(TRIANGLE, 'doc_james_6', 3, (130, 30, 70), (0, 0, 0)),
         #                  Pattern(TRIANGLE, 'doc_james_7', 3, (130, 30, 70), (0, 0, 0)),
         #                  Pattern(TRIANGLE, 'doc_james_8', 3, (130, 30, 70), (0, 0, 0)),
                          ]
+        
         self.lights = lights
         self.audio = audio
         self.midi = midi
